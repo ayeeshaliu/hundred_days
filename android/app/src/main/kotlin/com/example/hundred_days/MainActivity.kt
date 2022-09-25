857a78a4ce62bc121bc15443f2bc4009ac37e841
ayeeshaliu <90262228+ayeeshaliu@users.noreply.github.com>
2022-09-25 23:50:09 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/hundred_days/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hundred_days
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.grey[800],
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).viewInsets.vertical + 40,horizontal: MediaQuery.of(context).viewInsets.horizontal + 20 ),
+          child: Center(
+            child: Container(
+              height:400,
+              width: 300,
+              color: Colors.grey[700],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
